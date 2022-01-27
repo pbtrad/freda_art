d95002f6ff7f7a38d2d0474e2b69102e95231592
paul <paulbrowne2@gmail.com>
2022-01-27 21:37:25 +0000

    Jsx prettified for readability

--- src/components/About.js
+++ src/components/About.js
@@ -1,43 +1,17 @@
-import React from 'react';
-import './About.css';
+import React from "react";
+import "./About.css";
 
 const About = () => {
-    return (
-        <div className='section-container'>
-            
-
-<div className='about-main'>
-                <div className='about-main-left'>
-                    {/* Sub section 1 */}
-                    <h3 className='about-sub-head'>Artist</h3>
-                    <p className='about-details'>
-                        About details
-                        
-                    </p>
-
-                    
-
-                    {/* Sub section 3 */}
-                    {/*<h3 className='about-sub-head'>Public Speaker</h3>
-                    <p className='about-details'>
-                        Being a passionate public speaker, I've given talks at
-                        more than 75 events till now! Want me to speak in your
-                        event?{' '}
-                        <a
-                            className='about-link-element'
-                            href='mailto:info@theleanprogrammer.com'
-                        >
-                            Email me the details!
-                        </a>
-    </p>*/}
-                </div>
-
-                
-            </div>
-
-            
+  return (
+    <div className="section-container">
+      <div className="about-main">
+        <div className="about-main-left">
+          <h3 className="about-sub-head">Artist</h3>
+          <p className="about-details">About details</p>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default About;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,27 +1,25 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
-import './Home.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Home.css";
 
 const Home = () => {
-    return (
-        <div className="home-container">
-            <div className="header-text">
-                <h1>Freda Hatch</h1>
-                <p>Artist</p>
-            </div>
-            <div className="head-btns">
-                <Link to="/about" className="btn btn-white">
-                    <p className="btn-text">Know more about me</p>
-                </Link>
-                <Link to="/contact" className="btn btn-transparent">
-                <p className="btn-text">Connect with me</p>
-                </Link>
-            </div>
-            <div className="splash-image">
-            
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="home-container">
+      <div className="header-text">
+        <h1>Freda Hatch</h1>
+        <p>Artist</p>
+      </div>
+      <div className="head-btns">
+        <Link to="/about" className="btn btn-white">
+          <p className="btn-text">Know more about me</p>
+        </Link>
+        <Link to="/contact" className="btn btn-transparent">
+          <p className="btn-text">Connect with me</p>
+        </Link>
+      </div>
+      <div className="splash-image"></div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
