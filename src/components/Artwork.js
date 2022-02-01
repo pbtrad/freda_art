e9c6fbf8e6a7c17e1ed58ec3af3d305456049513
paul <paulbrowne2@gmail.com>
2022-02-01 23:24:46 +0000

    Added images for gallery

--- src/components/Artwork.js
+++ src/components/Artwork.js
@@ -4,29 +4,49 @@ import { SRLWrapper } from "simple-react-lightbox";
 function GalleryBox() {
  return (
    <SRLWrapper>
-     <div className="d-flex align-items-center">
-     <figure className="figure m-5">
-       <a className="img-fluid figure-img" href="https://github.com/pbtrad/Freda-Art/blob/master/assets/images/ballet.jpg?raw=true/400x300">
-         <img className="img-fluid figure-img" src="https://github.com/pbtrad/Freda-Art/blob/master/assets/images/ballet.jpg?raw=true/400x300" alt="lightbox" />
-       </a>
-       </figure>
-       <figure className="figure m-5">
-       <a className="img-fluid figure-img" href="https://github.com/pbtrad/Freda-Art/blob/master/assets/images/ballet.jpg?raw=true">
-         <img className="img-fluid figure-img" src="https://github.com/pbtrad/Freda-Art/blob/master/assets/images/ballet.jpg?raw=true" alt="lightbox" />
-       </a>
-       </figure>
-       <figure className="figure m-5">
-       <a className="img-fluid figure-img" href="https://github.com/pbtrad/Freda-Art/blob/master/assets/images/ballet.jpg?raw=true">
-         <img className="img-fluid figure-img" src="https://github.com/pbtrad/Freda-Art/blob/master/assets/images/ballet.jpg?raw=true" alt="lightbox" />
-       </a>
-       </figure>
-       <figure className="figure m-5">
-       <a className="img-fluid figure-img" href="https://github.com/pbtrad/Freda-Art/blob/master/assets/images/ballet.jpg?raw=true">
-         <img className="img-fluid figure-img" src="https://github.com/pbtrad/Freda-Art/blob/master/assets/images/ballet.jpg?raw=true" alt="lightbox" />
-       </a>
-       </figure>
-       
-     </div>
+     <div className="row">
+  <div className="col-lg-4 col-md-12 mb-4 mb-lg-0">
+    <img
+      src="https://github.com/pbtrad/Freda-Art/blob/master/assets/images/ballet.jpg?raw=true"
+      className="w-100 shadow-1-strong rounded mb-4"
+      alt="Ballet"
+    />
+
+    <img
+      src="https://github.com/pbtrad/Freda-Art/blob/master/assets/images/beluga.jpg?raw=true"
+      className="w-100 shadow-1-strong rounded mb-4"
+      alt="Beluga"
+    />
+  </div>
+
+  <div className="col-lg-4 mb-4 mb-lg-0">
+    <img
+      src="https://github.com/pbtrad/Freda-Art/blob/master/assets/images/cat-wth-pearl-earing.jpg?raw=true"
+      className="w-100 shadow-1-strong rounded mb-4"
+      alt="Cat with the pearl earing"
+    />
+
+    <img
+      src="https://github.com/pbtrad/Freda-Art/blob/master/assets/images/donkey-cat.jpg?raw=true"
+      className="w-100 shadow-1-strong rounded mb-4"
+      alt="Donkey Cat"
+    />
+  </div>
+
+  <div className="col-lg-4 mb-4 mb-lg-0">
+    <img
+      src="https://github.com/pbtrad/Freda-Art/blob/master/assets/images/garden.jpg?raw=true"
+      className="w-100 shadow-1-strong rounded mb-4"
+      alt="Garden"
+    />
+
+    <img
+      src="https://github.com/pbtrad/Freda-Art/blob/master/assets/images/horse-bike.jpg?raw=true"
+      className="w-100 shadow-1-strong rounded mb-4"
+      alt="Horse Bike"
+    />
+  </div>
+</div>
    </SRLWrapper>
  );
 }
