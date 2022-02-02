57eaa8c35face21ee53d48db435c4f86b51d8500
paul <paulbrowne2@gmail.com>
2022-02-02 19:13:28 +0000

    Added contact form

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -3,13 +3,45 @@ import "./Contact.css";
 
 const Contact = () => {
   return (
-    <div className="section-container">
-      <div className="contact-main">
-        <div className="contact-main-left">
-          <h3 className="contact-sub-head">Artist</h3>
-          <p className="contact-details">Contact details</p>
-        </div>
+    <div className="section-container text-center">
+      <h1 className="mt-5">Get in touch.</h1>
+
+      <div className="contact-form-container mx-auto">
+        <form
+          className="contact-form"
+          action="https://formspree.io/f/xbjqbagr"
+          method="POST"
+        >
+          {/* Email Id Input */}
+          <input
+            type="email"
+            placeholder="Your Email ID"
+            name="_replyto"
+            className="input-box email-input"
+            autoComplete="off"
+          />
+
+          {/* Email Body */}
+          <textarea
+            type="text"
+            placeholder="Your Message"
+            name="message"
+            className="input-box body-input"
+          ></textarea>
+
+          {/* Submit button */}
+          <button type="submit" className="contact-btn">
+            Send Email
+          </button>
+        </form>
       </div>
+
+      <div className="social-icons-container">
+        <a href="https://www.facebook.com/freda.hatch" className="social-icon">
+        <img src="https://img.icons8.com/fluency/48/000000/facebook-new.png"/>
+        </a>
+      </div>
+
     </div>
   );
 };
