95f9f5b791ed12bb4f1c099cecc92d55a3b948ec
paul <paulbrowne2@gmail.com>
2022-02-07 15:53:27 +0000

    Text dark added for nav links

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,7 +5,7 @@ import { Navbar, Nav, Offcanvas, Container } from "react-bootstrap";
 const Header = () => {
   return (
     <div>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <nav className="navbar navbar-expand-lg navbar-light bg-transparent">
   <div className="container-fluid">
     <a className="navbar-brand" href="#home"></a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -14,16 +14,16 @@ const Header = () => {
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <a className="nav-link" aria-current="page" href="#home">Home</a>
+          <a className="nav-link text-dark" aria-current="page" href="#home">Home</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#about">About</a>
+          <a className="nav-link text-dark" href="#about">About</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#artwork">Art</a>
+          <a className="nav-link text-dark" href="#artwork">Art</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#contact">Contact</a>
+          <a className="nav-link text-dark" href="#contact">Contact</a>
         </li>
       </ul>
       
